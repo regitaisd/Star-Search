93ff2f6b91ea982a3424d0be418ef8bebaeba3a6
Mikha Yupikha <Home@s-MacBook-Pro-15-Retina.local>
2016-10-03 16:15:27 +0700

    first commit

--- main.cpp
+++ main.cpp
@@ -9,3 +9,39 @@
 #include <iostream>
 using namespace std;
 
+
+void getJudgeData ();
+
+int main ()
+{
+    cout<< "Please input five judge scores: ";
+    getJudgeData();
+    return 0;
+    
+}
+
+void getJudgeData ()
+{
+    float number1, number2, number3, number4, number5, totalscore;
+    
+    cin>>  number1 >> number2 >> number3 >> number4 >> number5;
+    
+    if (number1<0 || number2<0 || number3<0 || number4<0 || number5<0)
+    {
+        cout<< "Scores cannot be less than 0";
+        return;
+    }
+    else if (number1>10 || number2>10 || number3>10 || number4>10 || number5>10)
+    {
+        cout<< "Scores cannot be more than 10.";
+        return;
+    }
+    else
+    {
+        cout<< "congrats.";
+        return;
+    }
+    
+    return;
+}
+
