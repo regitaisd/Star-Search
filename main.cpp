b93a2b361914090117b44917089ce78888566027
mikhaputri <mikhayupikha@yahoo.com>
2016-10-03 11:54:13 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  Function
+//
+//  Created by Mikha Yupikha on 03/10/2016.
+//  Copyright © 2016 Mikha Yupikha. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
